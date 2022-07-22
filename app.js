065057edb2ea1bb09354cdb2ed7ebfbf45537f9b
Rohan Shrestha <rohanstha000@gmail.com>
2022-07-22 19:19:59 +0545

    changed direct route

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 require('express-async-errors');
 
 //security package
-const helmet = require('helmet')
-const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
 const express = require('express');
 const app = express();
 
@@ -24,16 +26,17 @@ app.set('trust proxy',1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, //100requests per window
-}))
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-app.use(helmet())
-app.use(cors())
-app.use(xss())
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
-app.get('/',(req,res)=>{
-  res.send('jobs api');
-})
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authenticateUser, jobsRouter)
